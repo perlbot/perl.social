8801f26bcadeb672f2271fda6543e6e7fb79f95e
Ryan Voots <simcop2387@simcop2387.info>
2021-06-02 11:52:37 -0400

    carton setup and cache

---
+++ README.md
@@ -0,0 +1,6 @@
+How to use
+
+carton install
+carton exec ./build.pl
+
+view site in rendered/
---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -eux
+
+mkdir rendered/ || echo Rendered directory exists
+
