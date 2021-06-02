f0d24afe914deffab0a0ecf928b1d4ba9564d21e
Ryan Voots <simcop2387@simcop2387.info>
2021-06-02 12:15:51 -0400

    extranous option

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ set -eux
 
 mkdir rendered/ || echo Rendered directory exists
 
-carton exec -c 'bash -c "cd site; statocles build && statocles deploy"'
+carton exec 'bash -c "cd site; statocles build && statocles deploy"'
