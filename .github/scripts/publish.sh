c97359f56cc073f79a883abecb0399623aebaf6a
Ryan Voots <simcop2387@simcop2387.info>
2021-06-14 19:17:18 -0400

    try this?

--- .github/scripts/publish.sh
+++ .github/scripts/publish.sh
@@ -4,8 +4,9 @@ echo Checking event \[${GITHUB_EVENT_NAME}\] and \[${GITHUB_REF} == ${SITE_BRANC
 
 if [[ ${GITHUB_EVENT_NAME} == "push" && ${GITHUB_REF} == "refs/heads/${SITE_BRANCH}" ]]; then
   echo Running deploy job
-  git stash -u
+  #git stash -u
   cd site/
+  statocles build
   statocles deploy
 else
   echo Not a push to the site branch, not deploying
