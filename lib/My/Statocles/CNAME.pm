1c2d6da67646e596310d8d30e10bf736fea9f7e2
Ryan Voots <simcop2387@simcop2387.info>
2021-06-25 17:12:21 -0400

    missed these

--- lib/My/Statocles/CNAME.pm
+++ lib/My/Statocles/CNAME.pm
@@ -5,8 +5,6 @@ with 'Statocles::App';
 use Statocles::Page::Plain;
 
 sub pages {
-use Data::Dumper;
-print Dumper(\%ENV);
     return Statocles::Page::Plain->new(
         path => '/CNAME',
         content => $ENV{CNAME},
